fd93348aed46d8d3f673efe651f9e41f295cfc52
snirsnir <1technoda1@gmail.com>
2026-08-16 14:23:52 +0300

    Fix: Re-enable Splats to show world
    Keep aggressive pixelRatio reduction and FPS caps for performance.

--- quality.js
+++ quality.js
@@ -24,10 +24,10 @@
     var KEY = 'chef-quality';
 
     var PRESETS = {
-        normal:   { maxPixelRatio: 0.75, antialias: false, shadows: false, fillLights: false, splatsEnabled: false, maxFps: 60 },
-        low:      { maxPixelRatio: 0.5,  antialias: false, shadows: false, fillLights: false, splatsEnabled: false, maxFps: 45 },
-        ultra:    { maxPixelRatio: 0.35, antialias: false, shadows: false, fillLights: false, splatsEnabled: false, maxFps: 30 },
-        extreme:  { maxPixelRatio: 0.25, antialias: false, shadows: false, fillLights: false, splatsEnabled: false, maxFps: 15 }
+        normal:   { maxPixelRatio: 0.75, antialias: false, shadows: false, fillLights: false, splatsEnabled: true, maxFps: 60 },
+        low:      { maxPixelRatio: 0.5,  antialias: false, shadows: false, fillLights: false, splatsEnabled: true, maxFps: 45 },
+        ultra:    { maxPixelRatio: 0.35, antialias: false, shadows: false, fillLights: false, splatsEnabled: true, maxFps: 30 },
+        extreme:  { maxPixelRatio: 0.25, antialias: false, shadows: false, fillLights: false, splatsEnabled: true, maxFps: 15 }
     };
 
     // Downgrade thresholds: if fps < 20 go to ultra, if < 40 go to low
